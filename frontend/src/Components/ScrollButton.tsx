872eb415ff8fe8c5b126b893d423dd4c24029686
serAnkii <recruitankitnegi2023@gmail.com>
2023-10-11 19:32:21 +0530

    fixed scrollbutton.tsx

--- frontend/src/Components/ScrollButton.tsx
+++ frontend/src/Components/ScrollButton.tsx
@@ -1,43 +1,38 @@
-
-import { useEffect, useState  } from 'react';
+import { configureStore } from '@reduxjs/toolkit';
+import { useEffect, useState , useRef } from 'react';
 
 const ScrollButton = () => {
   const [scrollPosition, setScrollPosition] = useState(0);
-  const [windowSize, setWindowSize] = useState(window.innerWidth);
-
-  const moveBall = () => {
-    setScrollPosition(window.scrollY);
-    setWindowSize(window.innerWidth);
-  };
-
-  const handleScroll = () => {
-    moveBall();
-  };
-
-  const handleResize = () => {
-    moveBall();
-  };
+  const parentref = useRef(null)
 
   useEffect(() => {
-    handleScroll(); // Initial check
+    const moveball = ()=>{
+      setScrollPosition(window.scrollY)
+    }
+    const handleScroll = () => {
+      if(window.scrollY<=300)
+      {
+        window.addEventListener("scroll",moveball)
+      }
+      else{
+        window.removeEventListener("scroll",moveball)
+      }
+    };
 
-    window.addEventListener('scroll', handleScroll);
-    window.addEventListener('resize', handleResize);
-
+    window.addEventListener("scroll",handleScroll)
     return () => {
       window.removeEventListener('scroll', handleScroll);
-      window.removeEventListener('resize', handleResize);
     };
   }, []);
 
-  const dynamicValue = Math.min(scrollPosition, windowSize);
-  const translateYValue = -Math.cos(dynamicValue / 100) * 30;
+  const translateYValue = (-Math.cos(scrollPosition / 100) * 30);
   const circleStyle = {
     transform: `translateY(${translateYValue}px)`,
     transition: 'transform 0.2s ease-in',
   };
+
   return (
-    <div  className="relative">
+    <div ref={parentref} className="relative">
       <button className="flex items-center bg-transparent m-10 mx-auto">
         <div className="box">
           <div className="circle" style={circleStyle}></div>
