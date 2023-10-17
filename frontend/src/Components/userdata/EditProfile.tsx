7d3c9db50657a181a63b9ca8db6dfd698933a134
Ankush Negi <ankushnegi42001@gmail.com>
2023-10-17 12:00:27 +0530

    feat: Modified the profile page

--- frontend/src/Components/userdata/EditProfile.tsx
+++ frontend/src/Components/userdata/EditProfile.tsx
@@ -1,96 +1,170 @@
-const EditProfile = () => {
-  return (
-    <div className="flex-1 items-center justify-center p-5 overflow-y-auto">
-      <div className="bg-white p-8 rounded shadow-md w-full md:w-1/2 lg:w-1/3">
-        <h1 className="flex items-center justify-center text-2xl font-bold mb-10 underline">Edit Profile</h1>
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-        <form className="space-y-4">
-          <div className="flex flex-col">
-            <label htmlFor="username" className="mb-1 text-sm font-semibold">
-              Username
-            </label>
-            <input
-              type="text"
-              id="username"
-              className="p-2 border rounded"
-              placeholder="Enter your username"
-            />
+const EditProfile = () => {
+  const navigate = useNavigate();
+  const [showEditProfileButton, setShowEditProfileButton] = useState(false);
+  const [showResetPasswordButton, setShowResetPasswordButton] = useState(false);
+
+  const handleEditProfileHover = () => {
+    setShowEditProfileButton(true);
+  };
+
+  const handleEditProfileLeave = () => {
+    setShowEditProfileButton(false);
+  };
+
+  const handleResetPasswordHover = () => {
+    setShowResetPasswordButton(true);
+  };
+
+  const handleResetPasswordLeave = () => {
+    setShowResetPasswordButton(false);
+  };
+  const handleEditProfileClick = () => {
+    navigate('../profile');
+  };
+
+  const handleResetPasswordClick = () => {
+    navigate('../profile');
+  };
+
+  return (
+    <div className="w-full h-full flex-1 items-center justify-center p-5 overflow-y-scroll p-2 scroll-smooth border hover:border-richtextdark">
+      <header className="md:flex items-center justify-between mt-2 mb-3">
+        <div className="flex items-center mb-3">
+          <img
+            className="rounded-full mr-5 ml-8"
+            src="https://images.unsplash.com/photo-1617127365659-c47fa864d8bc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=120&h=120"
+            alt={"userData.username"}
+          />
+          <div className="flex items-center flex-wrap">
+            <h1 className="leading-tight flex-col">
+              <a href="./Profile" className="font-bold text-xl">Ankush Negi</a>
+              <div className="text-gray-600">@username</div>
+            </h1>
           </div>
+
+        </div>
+        <div className='mr-8'>
+          <a href="./profile" className="bg-richtextdark text-white rounded-lg px-4 py-2 text-md leading-5 hover:no-underline hover:border-white">Go to your profile</a>
+        </div>
+      </header>
+
+      <div
+        className="p-8 rounded shadow-md w-full h-full"
+        onMouseEnter={handleEditProfileHover}
+        onMouseLeave={handleEditProfileLeave}
+      >
+        <h1 className="flex items-center text-xl text-richtextdark font-medium mb-10 border-b">PROFILE DETAILS</h1>
+
+        <form className="space-y-6">
           <div className="flex flex-col">
-            <label htmlFor="username" className="mb-1 text-sm font-semibold">
+            <label htmlFor="Name" className="mb-1 text-md font-medium  text-white">
               Name
             </label>
             <input
               type="text"
               id="Name"
-              className="p-2 border rounded"
+              className="bg-transparent text-white p-2 border rounded-lg hover:border-richtextdark focus:border-richtextdark focus:outline-none text-md"
               placeholder="Enter your name"
             />
           </div>
-          <div className="mb-4">
-            <label htmlFor="dob" className="block text-sm font-semibold">
+          <div className="flex flex-col">
+            <label htmlFor="username" className="mb-1 text-md font-medium  text-white">
+              Username
+            </label>
+            <input
+              type="text"
+              id="username"
+              className="bg-transparent text-white p-2 border rounded-lg hover:border-richtextdark focus:border-richtextdark focus:outline-none text-md"
+              placeholder="Enter your username"
+            />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="dob" className="block text-md font-medium  text-white">
               Date of Birth
             </label>
             <input
               type="date"
               id="dob"
               name="dob"
-              className="mt-1 p-2 border border-gray-300 rounded w-full"
+              className="bg-transparent text-white p-2 border rounded-lg hover:border-richtextdark focus:border-richtextdark focus:outline-none text-md"
             />
           </div>
-
           <div className="flex flex-col">
-            <label htmlFor="email" className="mb-1 text-sm font-semibold">
+            <label htmlFor="email" className="mb-1 text-md font-medium  text-white">
               Email
             </label>
             <input
               type="email"
               id="email"
-              className="p-2 border rounded"
+              className="bg-transparent text-white p-2 border rounded-lg hover:border-richtextdark focus:border-richtextdark focus:outline-none text-md"
               placeholder="Enter your email"
             />
           </div>
-
           <div className="flex flex-col">
-            <label htmlFor="bio" className="mb-1 text-sm font-semibold">
+            <label htmlFor="bio" className="mb-1 text-md font-medium  text-white">
               Bio
             </label>
             <textarea
               id="bio"
-              className="p-2 border rounded"
-              placeholder="Write a short bio about yourself"
+              className="bg-transparent text-white p-2 border rounded-lg hover:border-richtextdark focus:border-richtextdark focus:outline-none text-md"
+              placeholder="Write a short bio about yourself... 😊"
             />
           </div>
 
-          <div className="flex flex-col">
-            <label htmlFor="password" className="mb-1 text-sm font-semibold">
-              New Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              className="p-2 border rounded"
-              placeholder="Enter your new password"
-            />
-          </div>
-          <div>
-            <label htmlFor="password" className="mb-1 text-sm font-semibold">
-              Re-enter New Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              className="p-2 border rounded"
-              placeholder="Enter your new password"
-            />
-          </div>
-          <div className="flex items-center justify-center">
-            <button
+          <div className="flex items-center justify-center ">
+            {showEditProfileButton && (
+              <button onClick={handleEditProfileClick}
               type="submit"
-              className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition"
+              className="bg-richtextdark text-white py-2 px-4 rounded-xl hover:bg-richtextdark transition"
             >
-              Save Changes
+              Save Details
             </button>
+            )}
+          </div>
+        </form>
+      </div>
+      <div
+        className="p-8 rounded shadow-md w-full h-full"
+        onMouseEnter={handleResetPasswordHover}
+        onMouseLeave={handleResetPasswordLeave}
+      >
+        <form className="">
+          <div className="pt-10 space-y-6">
+            <h1 className="flex items-center text-xl text-richtextdark font-medium mb-10 border-b">RESET PASSWORD</h1>
+            <div className="flex flex-col">
+              <label htmlFor="password" className="mb-1 text-md font-medium text-white">
+                New Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                className="bg-transparent text-white p-2 border rounded-lg hover:border-richtextdark focus:border-richtextdark focus:outline-none text-md"
+                placeholder="Enter your new password"
+              />
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="password" className="mb-1 text-md font-medium  text-white">
+                Re-enter New Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                className="bg-transparent text-white p-2 border rounded-lg hover:border-richtextdark focus:border-richtextdark focus:outline-none text-md"
+                placeholder="Enter your new password"
+              />
+            </div>
+          </div>
+
+          <div className="flex items-center justify-center mt-6 pb-6">
+           {showResetPasswordButton &&( <button onClick={handleResetPasswordClick}
+              type="submit"
+              className="bg-richtextdark text-white py-2 px-4 rounded-xl hover:bg-richtextdark transition"
+            >
+              Reset Password
+            </button>)}
           </div>
         </form>
       </div>
--- frontend/src/Components/userdata/ProfileHeader.tsx
+++ frontend/src/Components/userdata/ProfileHeader.tsx
@@ -10,9 +10,6 @@ type UserData = {
   Expertise?: string[];
   phone_number?: string;
   username?: string;
-  // followers?: string;
-  // following?: string;
-  // buddies?: string;
 };
 
 const ProfileHeader = () => {
@@ -32,7 +29,7 @@ const ProfileHeader = () => {
         console.error(error);
       }
     };
-  
+
     fetchData();
   }, [id]);
 
@@ -42,32 +39,38 @@ const ProfileHeader = () => {
         {userData && (
           <div className="profile relative mt-10 text-white">
             <div className="profile-image">
-              <img className="block rounded-full mx-20" src="https://images.unsplash.com/photo-1513721032312-6a18a42c8763?w=152&h=152&fit=crop&crop=faces" alt="Profile" />
+              <img className="block rounded-full mx-20" src="https://images.unsplash.com/photo-1617127365659-c47fa864d8bc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=130&h=130" alt="Profile" />
             </div>
 
-            <div className="flex items-center ">
-              <p className="text-2xl font-light mx-10 pl-20">{userData.name}</p>
-              <Link
-                to={'/dashboard/editProfile'}
-                className="bg-transparent cursor-pointer font-semibold  text-base leading-8 border-2 border-green-600 text-green-600 rounded-md px-6  hover:bg-green-600 hover:text-black hover:no-underline hover:border-green-600"
-              >
-                Edit Profile
-              </Link>
+            <div className="flex items-center  ">
+              <p className="text-2xl font-semibold mx-10 pl-20">Ankush Negi</p>
+              <div className=" mx-20 pl-20">
+                <Link
+                  to={'/dashboard/editProfile'}
+                  className="bg-transparent cursor-pointer font-semibold  text-base leading-8 border-2 border-green-600 text-green-600 rounded-md px-6 py-2  hover:bg-green-600 hover:text-black hover:no-underline hover:border-green-600"
+                >
+                  Edit Profile
+                </Link>
+              </div>
+
             </div>
 
             <div className="float-left font-semibold ml-10">
               <ul className="flex items-center mt-6">
-                <li className="inline-block text-base leading-6 mr-10 cursor-pointer">
-                  <span>{ userData.followers? userData.followers : 0}</span>  Followers
+                <span className="text-lg pr-3"> 100 </span>
+                <li className="inline-block text-lg text-richtextdark mr-10 cursor-pointer">
+                  Followers
                 </li>
-                <li className="inline-block text-base leading-6 cursor-pointer">
-                  <span> {userData.following ? userData.following :0} </span> Following
+                <span className="text-lg pr-3">100 </span>
+                <li className="inline-block text-lg text-richtextdark cursor-pointer">
+                  Following
                 </li>
               </ul>
             </div>
-            <div className="float-left text-base font-normal mt-6 ml-10">
+            <div className="float-left text-base font-normal mt-6 ml-10 border-b">
               <p>
-                <span className="font-semibold"> {userData.name} </span> {userData.bio}.
+                <span className="font-semibold"></span>
+                “It is not how much we have, but how much we enjoy that makes happiness” – By Charles Spurgeon.
               </p>
             </div>
           </div>
